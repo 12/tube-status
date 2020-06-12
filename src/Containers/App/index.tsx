6c902dd1fde9c4d2335d62a67f34c154d7161a64
Rhys Hellyar <rhys.hellyar@bbc.co.uk>
2020-06-12 16:23:34 +0100

    Filter necessary info out of response

--- src/Containers/App/index.tsx
+++ src/Containers/App/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Global } from '@emotion/core';
 import useFetch from '../../Helpers/useFetch';
 import Line, { LineProps } from '../../Components/Line/index';
@@ -14,7 +14,21 @@ interface ResponseType {
 }
 
 const App = () => {
+  const [data, setData] = useState<LineProps[] | null>(null);
   const res: ResponseType = useFetch(endpoint);
+
+  useEffect(() => {
+    if (!res.response) return;
+
+    // Filter what's needed. Should be easier to compare against and prevent re-renders.
+    const filteredResponse: LineProps[] = res.response.map(({ name, lineStatuses }) => ({
+      name,
+      key: name,
+      lineStatuses,
+    }));
+
+    setData(filteredResponse);
+  }, [res.response]);
 
   if (!res.response) {
     return <h1>Loading</h1>;
@@ -24,12 +38,12 @@ const App = () => {
     <>
       <Global styles={globalStyles} />
       <WrapperStyled>
-        {res.response &&
-          res.response.map(({ name, lineStatuses }: LineProps) => (
+        {data &&
+          data.map(({ name, lineStatuses }: LineProps) => (
             <Line key={name} name={name} lineStatuses={lineStatuses} />
           ))}
       </WrapperStyled>
-      {!!res.response && (
+      {!!data && (
         <GithubLinkStyled href="https://github.com/12/tube-status">
           Open source on Github!
         </GithubLinkStyled>
